13638b47b56f4379c69a31bebe944c0d077c428d
Steven Frazier <steven.frazier@usa.net>
2017-03-27 10:11:44 -0400

    Update entertainment_center.py

--- entertainment_center.py
+++ entertainment_center.py
@@ -8,40 +8,45 @@ import fresh_tomatoes
 import media
 
 # List of movies begins here.
-resident_evil_1 = media.Movie("Resident Evil",
-                        "A deadly virus may be released upon the world if an elite military team is unable to break into the secret complex of an evil corporation set out to achieve world domination.",
-                        "https://upload.wikimedia.org/wikipedia/en/a/a1/Resident_evil_ver4.jpg",
-                        "https://www.youtube.com/watch?v=lBxjZahSN7o")
+RESIDENT_EVIL = media.Movie("Resident Evil",
+                            "https://upload.wikimedia.org/wikipedia/en/a/a1/"+
+                            "Resident_evil_ver4.jpg",
+                            "https://www.youtube.com/watch?v=lBxjZahSN7o", " ")
+RESIDENT_EVIL.story_line = RESIDENT_EVIL.update_storyline()
 
-underworld = media.Movie("Underworld",
-                         "A beautiful vampire warrior, is entrenched in a war between the vampire and werewolf races.",
-                         "https://upload.wikimedia.org/wikipedia/en/c/cc/Underworld_%282003_film%29_poster.jpg",
-                         "https://www.youtube.com/watch?v=MqT-e44kIM8")
+UNDERWORLD = media.Movie("Underworld",
+                         "https://upload.wikimedia.org/wikipedia/en/c/cc/"+
+                         "Underworld_%282003_film%29_poster.jpg",
+                         "https://www.youtube.com/watch?v=MqT-e44kIM8", " ")
+UNDERWORLD.story_line = UNDERWORLD.update_storyline()
 
-star_wars = media.Movie("Star Wars",
-                        "Star Wars is a 1977 American epic space opera film written and directed by George Lucas.",
-                        "https://upload.wikimedia.org/wikipedia/en/8/87/StarWarsMoviePoster1977.jpg",
-                        "https://www.youtube.com/watch?v=K8AD7wzsPFQ")
+STAR_WARS = media.Movie("Star Wars",
+                        "https://upload.wikimedia.org/wikipedia/en/8/87/"+
+                        "StarWarsMoviePoster1977.jpg",
+                        "https://www.youtube.com/watch?v=K8AD7wzsPFQ", " ")
+STAR_WARS.story_line = STAR_WARS.update_storyline()
 
-star_trek = media.Movie("Star Trek",
-                        "Storyline",
-                        "https://upload.wikimedia.org/wikipedia/en/d/df/Star_Trek_The_Motion_Picture_poster.png",
-                        "https://www.youtube.com/watch?v=g0-7Z2lYII8")
+STAR_TREK = media.Movie("Star Trek",
+                        "https://upload.wikimedia.org/wikipedia/en/d/df/"+
+                        "Star_Trek_The_Motion_Picture_poster.png",
+                        "https://www.youtube.com/watch?v=g0-7Z2lYII8", " ")
+STAR_TREK.story_line = STAR_TREK.update_storyline()
 
-wizard_of_oz = media.Movie("Wizard of Oz",
-                           "A tornado wisks Dorothy and her dog, Toto, to the magical land of Oz.",
-                           "https://upload.wikimedia.org/wikipedia/commons/c/ca/WIZARD_OF_OZ_ORIGINAL_POSTER_1939.jpg",
-                           "https://www.youtube.com/watch?v=vkZcYMy85lY")
+WIZARD_OF_OZ = media.Movie("Wizard of Oz",
+                           "https://upload.wikimedia.org/wikipedia/commons/"+
+                           "c/ca/WIZARD_OF_OZ_ORIGINAL_POSTER_1939.jpg",
+                           "https://www.youtube.com/watch?v=vkZcYMy85lY", " ")
+WIZARD_OF_OZ.story_line = WIZARD_OF_OZ.update_storyline()
 
-terminator = media.Movie("Terminator",
-                         "In the not too distant future, artificial intelligence has taken over the world and only a small human resistence remains.  Disguised as a human, a cyborg assassin known as a Terminator travels from 2029 to 1984 to kill the mother of the future resistence leader. ",
-                         "https://upload.wikimedia.org/wikipedia/en/7/70/Terminator1984movieposter.jpg",
-                         "https://www.youtube.com/watch?v=QIcomuI1j7I")
+ODD_THOMAS = media.Movie("Odd Thomas",
+                         "https://upload.wikimedia.org/wikipedia/en/f/ff/"+
+                         "Odd_Thomas_poster_film.jpg",
+                         "https://www.youtube.com/watch?v=5ybBq5AETyU", " ")
+ODD_THOMAS.story_line = ODD_THOMAS.update_storyline()
 # List of movies ends here.
 
-movies = [terminator, star_wars, underworld, star_trek,
-          resident_evil_1, wizard_of_oz]
+MOVIES = [ODD_THOMAS, STAR_WARS, UNDERWORLD, STAR_TREK,
+          RESIDENT_EVIL, WIZARD_OF_OZ]
 
-fresh_tomatoes.open_movies_page(movies)     # Creates an HTML page
-                                            # and opens in a Web browser
-
+# Creates an HTML page and opens in a Web browser
+fresh_tomatoes.open_movies_page(MOVIES)
